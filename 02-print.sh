6b18e3cb009e058363299f38618f5a115346fcdc
sruthilaya <sruthilayagadila@gmail.com>
2023-05-29 16:28:29 +0530

    frontend

--- 02-print.sh
+++ 02-print.sh
@@ -5,13 +5,13 @@
 echo hello world
 
 #To print colour codes
-# syntax echo -e "\e[COLmMESSAGE\E[0m"
+# syntax echo -e "\e[COLmMESSAGE\e[0m"
 
-echo -e "\e[31m hello world from red\E[0m"
-echo -e "\e[32m hello world from green\E[0m"
-echo -e "\e[33m hello world from yellow\E[0m"
-echo -e "\e[34m hello world from blue\E[0m"
-echo -e "\e[35m hello world from magenta\E[0m"
-echo -e "\e[36m hello world from cyan\E[0m"
+echo -e "\e[31m hello world from red\e[0m"
+echo -e "\e[32m hello world from green\e[0m"
+echo -e "\e[33m hello world from yellow\e[0m"
+echo -e "\e[34m hello world from blue\e [0m"
+echo -e "\e[35m hello world from magenta\e[0m"
+echo -e "\e[36m hello world from cyan\e[0m"
 
 echo changes made to reflect on server side file
