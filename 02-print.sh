671044ecd86a39f6eb583cdee8128510819e258b
sruthilaya <sruthilayagadila@gmail.com>
2023-05-29 15:55:13 +0530

    printing and comments

--- 02-print.sh
+++ 02-print.sh
@@ -13,3 +13,5 @@ echo -e "\e[33m hello world from yellow\E[0m"
 echo -e "\e[34m hello world from blue\E[0m"
 echo -e "\e[35m hello world from magenta\E[0m"
 echo -e "\e[36m hello world from cyan\E[0m"
+
+echo changes made to reflect on server side file
