7ffc2097ee0767cf414309f59176c0c0f71c6656
sruthilaya <sruthilayagadila@gmail.com>
2023-05-29 15:50:09 +0530

    printing and comments

---
+++ 02-print.sh
@@ -0,0 +1,15 @@
+# to print comments we use '#' before statements
+
+#to print statement we use command called echo
+
+echo hello world
+
+#To print colour codes
+# syntax echo -e "\e[COLmMESSAGE\E[0m"
+
+echo -e "\e[31m hello world from red\E[0m"
+echo -e "\e[32m hello world from green\E[0m"
+echo -e "\e[33m hello world from yellow\E[0m"
+echo -e "\e[34m hello world from blue\E[0m"
+echo -e "\e[35m hello world from magenta\E[0m"
+echo -e "\e[36m hello world from cyan\E[0m"
